0168a59193032a271e7de5cf8385edbd5352ccfc
MashR3 <michaelash17@googlemail.com>
2017-10-19 09:27:10 +0100

    Done

--- spec/json_test_spec.rb
+++ spec/json_test_spec.rb
@@ -16,11 +16,19 @@ describe 'JSON example tests' do
   end
 
   it "the colour red should have the hex value of '#FF0' " do
-    expect @json['colors'][Array].any? { |i| ["red", "FF0"].include? i }
+    # expect @json['colors'][Array].any? { |i| ["red", "FF0"].include? i }
+    @json['colors'].each do |i|
+      if i|"color"| == "red"
+        expect(i["code"]["hex"]).to eq("#FF0")
+      end
   end
 
   it "the colour green should have the RGBA value of '0,255,0,1' " do
-    expect @json['colors'][Array].any? { |i| ["green", "0,255,0,1"].include? i }
+    # expect @json['colors'][Array].any? { |i| ["green", "0,255,0,1"].include? i }
+    @json['colors'].each do |i|
+      if i|"color"| == "red"
+        expect(i["code"]["rgba"]).to eq([0,255,0,1])
+      end
   end
 
 end
